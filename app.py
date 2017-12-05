ac2470d5dba80ed890380562d1815648c4f00dd8
razorhollow <rob@razorhollow.com>
2017-12-05 13:44:50 -0500

    Beginning html and css.

---
+++ app.py
@@ -0,0 +1,11 @@
+#!python3
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+	return render_template('index.html')
+
+app.run()	
