4fd5775656841a2923eeb2f6b3fef759141f7360
razorhollow <rob@razorhollow.com>
2017-12-06 16:11:03 -0500

    Add Holiday Calendar.

--- functions.py
+++ functions.py
@@ -1,39 +1,7 @@
 #!python3
-from flask import Flask, request, render_template
 
 import datetime
-
 from peewee import *
-
-app = Flask(__name__)
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-
-	return render_template('index.html')
-
-@app.route('/holiday')
-def holiday():
-	today = datetime.date.today()
-	thisYear = today.year
-	query =  HolidayCalendar.select()
-	return render_template('holiday.html',
-							query=query,
-							thisYear=thisYear)
-
-@app.route('/vacation')
-def vacation():
-	return render_template('vacation.html')
-
-@app.before_request
-def before_request():
-	db.connect()
-
-@app.after_request
-def after_request(response):
-	db.close()
-	return response
 
 db = SqliteDatabase('TimeOff.db')
 
@@ -61,5 +29,15 @@ def initialize():
     db.connect()
     db.create_tables([Employee, Vacation, HolidayCalendar], safe=True)
 
-isAdmin = [33, 65]
-app.run()	
+
+def getHolidayCal():
+	today = datetime.date.today()
+	thisYear = today.year
+	query =  HolidayCalendar.select()
+	for holday in query:
+		if holday.holidayDate.year == thisYear:
+			return ("{} | {}/{}/{}".format(holday.description, holday.holidayDate.month, holday.holidayDate.day, holday.holidayDate.year))
+
+initialize()
+getHolidayCal()
+db.close()
